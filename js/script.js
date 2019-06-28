8d6e16c3ccf7e2eb44e36a3c1df1ae2c43d258f6
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-06-27 23:28:36 -0400

    Changes checkboxes to Textbox and changed form design

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
+'use strict';
+
+function searchParks(states, maxParks){
+
+    const apiKey = "X6gU1HR9voYTM2Thisadb19sGIz2AjsTqdE1Jcim";
+    const query = `stateCode=${states}&limit=${maxParks}`;
+
+    console.log(`https://developer.nps.gov/api/v1/parks?api_key=${apiKey}&${query}`);
+    fetch(`https://developer.nps.gov/api/v1/parks?api_key=${apiKey}&${query}`)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson));
+
+}
+
+
 function watchForm(){
 
     $('.search-form').on('submit', function(e){
         e.preventDefault();
-        let selectedStates = $('.checkbox label input:checked').val();
-        console.log(selectedStates);
-    })
+
+        let maxParks = $('.max-parks').val();
+        
+        // get states entered by user in the input box
+        let states = $('.states').val();
+        
+        // removes all commas and replaces with %20C%20 and gets rids of all spaces
+        states = (states.replace(/ +/g, "")).replace(/,/g,"%2C");
+
+        console.log(states);
+        
+        // call back for the function to search for parks
+        searchParks(states, maxParks);
+        
+    });
 }
 
 $(watchForm);
